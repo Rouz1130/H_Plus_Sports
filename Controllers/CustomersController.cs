07beaeac163baa9f60c703730b80b9e2772a1ce1
Rouz Majlessi <rmajlessi@gmail.com>
2018-06-13 21:40:02 -0700

    Add customer controller with empty endpoints

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace H_Plust_Sports.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Customers")]
+    public class CustomersController : Controller
+    {
+        //Constructor
+        public CustomersController()
+        {
+
+        }
+
+        [HttpGet]
+        public IActionResult GetCustomer()
+        {
+           return Ok();
+        }
+
+        [HttpGet("{Id}")]
+        public IActionResult GetCustomer([FromRoute] int id)
+        {
+            return Ok(); 
+        }
+
+        [HttpPost]
+        public IActionResult PostCustomer([FromBody] Object obj)
+        {
+            return Ok();
+        }
+
+        [HttpPut("{id})")]
+        public IActionResult PutCustomer([FromRoute] int id, [FromBody] Object obj)
+        {
+            return Ok();
+        }
+
+        [HttpDelete("{id})")]
+        public IActionResult DeleteCustomer([FromRoute]int id)
+        {
+            return Ok();
+        }
+
+    }
+}
