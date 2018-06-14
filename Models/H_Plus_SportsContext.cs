44e6f1345311b34187dbc4d99ce52ac5d2cec95d
Rouz Majlessi <rmajlessi@gmail.com>
2018-06-13 21:16:17 -0700

    Add constructor for Dbcontext class for H_plust type

--- Models/H_Plus_SportsContext.cs
+++ Models/H_Plus_SportsContext.cs
@@ -12,13 +12,10 @@ namespace H_Plust_Sports.Models
         public virtual DbSet<Product> Product { get; set; }
         public virtual DbSet<Salesperson> Salesperson { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        public H_Plus_SportsContext(DbContextOptions<H_Plus_SportsContext> options)
+            : base(options)
         {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer(@"Server=tcp:hsportsmajlessi.database.windows.net,1433;Initial Catalog=H_Plus_Sports;Persist Security Info=False;User ID=rouzMajlessi;Password=Rouz2416!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
-            }
+
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
