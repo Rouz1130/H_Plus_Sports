8a76600ede555d3ebd69c35350dce0eecda9bb73
Rouz Majlessi <rmajlessi@gmail.com>
2018-06-15 01:04:35 -0700

    Add customer repository with stubs for time being

---
+++ Repositories/CustomerRepository.cs
@@ -0,0 +1,42 @@
+﻿using H_Plus_Sports.Interfaces;
+using H_Plus_Sports.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace H_Plus_Sports.Repositories
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        public Task<Customer> Add(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Exists(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Customer> Find(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Customer> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Customer> Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Customer> Update(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
